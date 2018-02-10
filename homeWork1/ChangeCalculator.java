71bd854275b299665ffaf85433708d35c0ee8988
kbml <kbml@faceless.com>
2018-02-09 20:11:51 -0800

    Changing some strings, minor things.

--- homeWork1/ChangeCalculator.java
+++ homeWork1/ChangeCalculator.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class ChangeCalculator {
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to the Change Calculator \n");
-
+		System.out.println("Welcome to the Change Calculator. \n");
 		Scanner sc = new Scanner(System.in);
-
 		String user_choice = "y";
 
 		while (user_choice.equalsIgnoreCase("y")) {
 			// input from the user
-			System.out.println("Enter number of cents (0-99): ");
+			System.out.println("Please enter number of cents (0-99): ");
 			int total_cents = sc.nextInt();
 			// calculate quarters
 			int quarters = total_cents / 25;
